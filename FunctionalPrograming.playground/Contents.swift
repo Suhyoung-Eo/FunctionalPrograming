4453a9bba2c232a2eae6c80243da4e980cdc39e2
Matthew <eossu@kakao.com>
2022-03-19 23:31:10 +0900

    feat: 추가 - Composition (함수의 합성)

--- FunctionalPrograming.playground/Contents.swift
+++ FunctionalPrograming.playground/Contents.swift
@@ -40,7 +40,7 @@ let area2 = x10(20)
 print(area2)
 
 
-//MARK: - 고차함수를 사용하여 짝수만 합하기
+//MARK: - Higher-Order Function (고차함수를 사용하여 짝수만 합하기)
 
 let array: [Int] = [1, 2, 3]
 
@@ -63,3 +63,60 @@ func solution(_ nums: [Int]) -> Int {
 }
 
 print(solution(array))
+
+
+//MARK: - Composition (함수의 합성)
+
+func f1(_ i: Int) -> Int {
+    return i * 2
+}
+
+func f2(_ i: Int) -> String {
+    return "\(i)"
+}
+
+let result = f2(f1(100))
+
+print(result)
+
+func ff(_ pf1: @escaping (Int) -> Int, _ pf2: @escaping (Int) -> String) -> (Int) -> String {
+    return { i in
+        return pf2(pf1(i))
+    }
+}
+
+let f3 = ff(f1, f2)
+let result3 = f3(100)
+
+print(result3)
+
+func comp<A, B, C>(_ pf1: @escaping (A) -> B, _ pf2: @escaping (B) -> C) -> (A) -> C {
+    return { i in
+        return pf2(pf1(i))
+    }
+}
+
+let f4 = comp(f1, f2)
+let result4 = f4(100)
+
+print(result4)
+
+/* 함수내부 구현*/
+
+let numbers = [ 1, 2, 3, 4, 6]
+
+func filterEven(_ nums: [Int]) -> [Int] {
+    return nums.filter { $0 % 2 == 0 }
+}
+
+func sum(_ nums: [Int]) -> Int {
+    return nums.reduce(0, +)
+}
+
+let filteredSum = comp(filterEven, sum)
+
+func solution1(_ nums: [Int]) -> Int {
+    return filteredSum(nums)
+}
+
+print(solution1(numbers))
