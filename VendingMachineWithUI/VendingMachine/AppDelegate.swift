834a7b2317cfa7f9a598ae6af78bda819e97a4b2
Matthew <eossu@kakao.com>
2022-03-28 15:33:30 +0900

    Style: Vending machine UI

---
+++ VendingMachineWithUI/VendingMachine/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VendingMachine
+//
+//  Created by bearfried on 2018. 8. 27..
+//  Copyright © 2018년 bearfried. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ VendingMachineWithUI/VendingMachine/ViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - UI
+
+    @IBOutlet weak var displayMoney: UILabel!
+
+    @IBOutlet weak var productOut: UIImageView!
+
+    @IBOutlet weak var textInfo: UILabel!
+
+    @IBAction func money100(_ sender: Any) {
+    }
+
+    @IBAction func money500(_ sender: Any) {
+    }
+
+    @IBAction func money1000(_ sender: Any) {
+    }
+
+    @IBAction func selectCola(_ sender: Any) {
+    }
+
+    @IBAction func selectCider(_ sender: Any) {
+    }
+
+    @IBAction func selectFanta(_ sender: Any) {
+    }
+
+    @IBAction func reset(_ sender: Any) {
+    }
+
+}
+
